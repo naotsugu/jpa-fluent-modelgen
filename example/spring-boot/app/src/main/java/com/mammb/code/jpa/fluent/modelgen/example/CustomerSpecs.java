73b1cc5327d98709bca38b35d437f58e8790a2a7
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2022-05-27 22:36:35 +0900

    Modify example

--- example/spring-boot/app/src/main/java/com/mammb/code/jpa/fluent/modelgen/example/CustomerSpecs.java
+++ example/spring-boot/app/src/main/java/com/mammb/code/jpa/fluent/modelgen/example/CustomerSpecs.java
@@ -3,21 +3,22 @@ package com.mammb.code.jpa.fluent.modelgen.example;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.Root;
+import java.util.regex.Pattern;
 
 public class CustomerSpecs {
 
     public static Specification<Customer> firstNameLike(String firstName) {
-        return (root, query, cb) -> cb.like(on(root).getFirstName(), '%' + firstName + '%');
+        return (root, query, cb) -> cb.like(on(root).getFirstName(), partial(firstName), '\\');
     }
 
     public static Specification<Customer> lastNameLike(String lastName) {
-        return (root, query, cb) -> cb.like(on(root).getLastName(), '%' + lastName + '%');
+        return (root, query, cb) -> cb.like(on(root).getLastName(), partial(lastName), '\\');
     }
 
     public static Specification<Customer> organizationNameLike(String name) {
         return (root, query, cb) -> {
             query.distinct(true);
-            return cb.like(on(root).joinOrganizations().getName(), '%' + name + '%');
+            return cb.like(on(root).joinOrganizations().getName(), partial(name), '\\');
         };
     }
 
@@ -32,4 +33,8 @@ public class CustomerSpecs {
         return new Customer_Root_(root);
     }
 
+    static Pattern ESCAPE_PATTERN = Pattern.compile("([%_\\\\])");
+    static String partial(String str) {
+        return "%" + ESCAPE_PATTERN.matcher(str).replaceAll("\\\\$1") + "%";
+    }
 }
